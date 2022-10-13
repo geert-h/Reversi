3988c7c43e717b3e4df49239d24a31a4e8aa6c46
geert-h <115552960+geert-h@users.noreply.github.com>
2022-10-13 13:22:07 +0200

    Add files via upload

---
+++ Reversi IMP/Reversi IMP/CellStateClass.cs
@@ -0,0 +1,12 @@
+﻿namespace Reversi_IMP
+{
+    enum CellState
+    {
+        None = 0,
+        Player1,
+        Player2
+    }
+}
+
+
+
---
+++ Reversi IMP/Reversi IMP/CheckCellsClass.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Reversi_IMP
+{
+    internal partial class Program
+    {
+        public partial class Reversi : Form
+        {
+            void CheckCells()
+            {
+                if (move % 2 == 0)
+                {
+                    if (table[Column, Row] == 1 || table[Column, Row] == 0)
+                    {
+                    }
+                    else
+                    {
+                        table[Column, Row] = 1;
+
+                        move++;
+                    }
+
+                }
+                else
+                {
+                    if (table[Column, Row] == 1 || table[Column, Row] == 0)
+                    {
+                    }
+                    else
+                    {
+                        table[Column, Row] = 0;
+                        move++;
+                    }
+                }
+            }
+        }
+    }
+}
